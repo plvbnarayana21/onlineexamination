f04ba48aaa6365e392aea1106a1980d2ad3900bf
plvbnarayana21 <114160138+plvbnarayana21@users.noreply.github.com>
2023-07-10 17:04:21 +0530

    Add files via upload

---
+++ Online_examination_system/src/animal.java
@@ -0,0 +1,70 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+class animal {
+double color;
+double sound;
+double size;
+public void difference() {
+}
+}
+
+class cat extends animal{
+    double color;
+    double sound;
+    double size;
+    public void difference(){
+        this.color=black;
+        this.sound=meo meo meo:
+        this.size=short;
+    }
+    void print() {
+        System.out.println("color=" + this.color, "sound=" + this.sound, "size=" + this.size);
+    }
+
+
+    class dog extends animal{
+        double color=white;
+        double sound =bou bou bou bou;
+        double size=large;
+    }
+    void print(){
+        System.out.println("color="+ this.color,"sound="+ this.sound,"size="+ this.size);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
